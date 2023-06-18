453e3470887d5e0f1d4c19838a7ef09b7b8a93e7
Alexandr Moroz <amoroz@griddynamics.com>
2023-06-18 17:58:22 +0000

    Q15 extract createTree to common utils

--- faang/15.maximum-depth-of-binary-tree/main.go
+++ faang/15.maximum-depth-of-binary-tree/main.go
@@ -2,42 +2,8 @@ package main
 
 import (
 	"fmt"
+	. "godsa/faang/utils"
 )
-
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
-func convertToTreeNode(m []interface{}) []*TreeNode {
-	r := make([]*TreeNode, len(m))
-	for i, v := range m {
-		if v != nil {
-			t := v.(int)
-			r[i] = &TreeNode{Val: t, Left: nil, Right: nil}
-		}
-	}
-	return r
-}
-
-func createTree(m []interface{}) *TreeNode {
-	nodes := convertToTreeNode(m)
-	kids := nodes[1:]
-
-	for i := range nodes {
-		if nodes[i] != nil {
-			if len(kids) > 0 {
-				nodes[i].Left, kids = kids[0], kids[1:]
-			}
-			if len(kids) > 0 {
-				nodes[i].Right, kids = kids[0], kids[1:]
-			}
-		}
-	}
-
-	return nodes[0]
-}
 
 func max(x int, y int) int {
 	if x > y {
@@ -59,7 +25,7 @@ func main() {
 	// m := []interface{}{3, 9, 20, nil, nil, 15, 7}
 	m := []interface{}{5, 4, 7, 3, nil, 2, nil, -1, nil, 9}
 	// m := []interface{}{nil}
-	p := createTree(m)
+	p := CreateTree(m)
 	d := maxDepth(p)
 	fmt.Println(p, d)
 }
---
+++ faang/utils/utils.go
@@ -0,0 +1,36 @@
+package utils
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func convertToTreeNode(m []interface{}) []*TreeNode {
+	r := make([]*TreeNode, len(m))
+	for i, v := range m {
+		if v != nil {
+			t := v.(int)
+			r[i] = &TreeNode{Val: t, Left: nil, Right: nil}
+		}
+	}
+	return r
+}
+
+func CreateTree(m []interface{}) *TreeNode {
+	nodes := convertToTreeNode(m)
+	kids := nodes[1:]
+
+	for i := range nodes {
+		if nodes[i] != nil {
+			if len(kids) > 0 {
+				nodes[i].Left, kids = kids[0], kids[1:]
+			}
+			if len(kids) > 0 {
+				nodes[i].Right, kids = kids[0], kids[1:]
+			}
+		}
+	}
+
+	return nodes[0]
+}
