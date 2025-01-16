dd564eb0c6c3c352f5849a940c38814a26e9f775
Alexandr Moroz <afmoroz@gmail.com>
2025-01-16 10:02:47 +0000

    62. Unique Paths

---
+++ leetcode/62.UniquePaths/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func uniquePaths(m int, n int) int {
+	d := make([][]int, m)
+	for i := range d {
+		d[i] = make([]int, n)
+		for j := range d[i] {
+			d[i][j] = 1
+		}
+	}
+	fmt.Println(d)
+	for col := 1; col < m; col++ {
+		for row := 1; row < n; row++ {
+			d[col][row] = d[col-1][row] + d[col][row-1]
+		}
+	}
+	fmt.Println(d)
+	return d[m-1][n-1]
+}
+
+func main() {
+	m, n := 3, 7
+	fmt.Println(uniquePaths(m, n))
+}
