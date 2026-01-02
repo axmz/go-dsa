cad020243f639392f457792509f1f24a5891eb1a
Alexandr Moroz <afmoroz@gmail.com>
2026-01-02 08:09:40 +0000

    cleanup bst

--- utils/bst/bst.go
+++ utils/bst/bst.go
@@ -5,89 +5,98 @@ import (
 	"fmt"
 )
 
-// BST structure
-type BST struct {
-	Root *Node
+// TreeNode structure of the node
+type TreeNode struct {
+	Value int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
-// Insert inserts the root node or passes the insert responsibility to the next node
-func (t *BST) Insert(num int) {
-	if t.Root == nil {
-		t.Root = &Node{num, nil, nil}
+// Insert inserts a value into BST
+func (n *TreeNode) Insert(num int) {
+	if n == nil {
+		return
+	}
+	if num > n.Value {
+		if n.Right == nil {
+			n.Right = &TreeNode{Value: num}
+		} else {
+			n.Right.Insert(num)
+		}
 	} else {
-		t.Root.Insert(num)
+		if n.Left == nil {
+			n.Left = &TreeNode{Value: num}
+		} else {
+			n.Left.Insert(num)
+		}
 	}
 }
 
-// Lookup passes the search to the root node
-func (t BST) Lookup(num int) (node *Node, parentNode *Node, err error) {
-	if t.Root == nil {
-		return node, parentNode, fmt.Errorf("Has no root")
+// Lookup searches for a value in BST and returns the node and its parent
+func (n *TreeNode) Lookup(num int, parent *TreeNode) (*TreeNode, *TreeNode, error) {
+	if n == nil {
+		return nil, parent, fmt.Errorf("node not found")
 	}
-	return t.Root.Lookup(num, t.Root)
+	if n.Value == num {
+		return n, parent, nil
+	}
+	if num > n.Value {
+		return n.Right.Lookup(num, n)
+	}
+	return n.Left.Lookup(num, n)
 }
 
-// Remove removes the node from the tree
-func (t *BST) Remove(num int) (node *Node, parentNode *Node, err error) {
-	node, parentNode, err = t.Lookup(num)
-
+// Remove removes the node with the given value from the tree
+func (n *TreeNode) Remove(num int) error {
+	node, parent, err := n.Lookup(num, nil)
 	if err != nil {
-		println(err.Error())
-		return node, parentNode, err
+		return err
 	}
 
-	if node == parentNode { // if root case
-		t.Root = nil
-	} else if node.Right == nil && node.Left == nil { // no right, no left - remove entirely
-		if parentNode.Right.Value == node.Value {
-			parentNode.Right = nil
-		}
-		if parentNode.Left.Value == node.Value {
-			parentNode.Left = nil
-		}
-	} else if node.Right == nil { // no right - connect parent with left
-		if parentNode.Right.Value == node.Value {
-			parentNode.Right = node.Left
-		}
-		if parentNode.Left.Value == node.Value {
-			parentNode.Left = node.Left
-		}
-	} else if node.Right != nil { // right - connect parent with leftmost
-		leftmost := node.Right
-		for {
-			if leftmost.Left != nil {
-				leftmost = leftmost.Left
-			} else {
-				break
-			}
-		}
-		if parentNode.Right.Value == node.Value {
-			parentNode.Right = leftmost
-		}
-		if parentNode.Left.Value == node.Value {
-			parentNode.Left = leftmost
+	// Can't remove root without returning new root
+	if parent == nil {
+		return fmt.Errorf("cannot remove root node directly")
+	}
+
+	var replacement *TreeNode
+
+	// Determine replacement node
+	if node.Left == nil && node.Right == nil {
+		replacement = nil // Leaf node
+	} else if node.Right == nil {
+		replacement = node.Left // Only left child
+	} else if node.Left == nil {
+		replacement = node.Right // Only right child
+	} else {
+		// Both children exist: find inorder successor (leftmost in right subtree)
+		replacement = node.Right
+		for replacement.Left != nil {
+			replacement = replacement.Left
 		}
 	}
 
-	return node, parentNode, err
+	// Update parent's pointer
+	if parent.Left == node {
+		parent.Left = replacement
+	} else {
+		parent.Right = replacement
+	}
+
+	return nil
 }
 
-// Print - prints the entire tree in json
-func (t BST) Print() {
-	printTree, _ := json.MarshalIndent(t, "", "|  ")
+// Print - prints the node in json
+func (n *TreeNode) Print() {
+	printTree, _ := json.MarshalIndent(n, "", "|  ")
 	fmt.Println(string(printTree))
 }
 
-// NewBst initializes an empty Bst
-func NewBst() *BST {
-	return &BST{
-		Root: nil,
+// BFS traverses the tree breadth-first
+func (n *TreeNode) BFS() []int {
+	if n == nil {
+		return []int{}
 	}
-}
-
-// BFS traverses the tree
-func (t *BST) BFS() []int {
-	queue := []*Node{t.Root}
+	queue := []*TreeNode{n}
 	list := []int{}
 	for len(queue) > 0 {
 		f := queue[0]
@@ -103,79 +112,118 @@ func (t *BST) BFS() []int {
 	return list
 }
 
-// BFSR recursively traverses the tree
-func (t *BST) BFSR(queue []*Node, list []int) []int {
-	if queue == nil {
-		queue = []*Node{t.Root}
-		list = []int{}
+// BFSR recursively traverses the tree breadth-first
+func (n *TreeNode) BFSR() []int {
+	if n == nil {
+		return []int{}
 	}
+	return n.bfsrHelper([]*TreeNode{n}, []int{})
+}
 
+func (n *TreeNode) bfsrHelper(queue []*TreeNode, list []int) []int {
 	if len(queue) == 0 {
 		return list
 	}
 
-	f := queue[0]
-	if f.Left != nil {
-		queue = append(queue, f.Left)
-	}
-	if f.Right != nil {
-		queue = append(queue, f.Right)
-	}
-	list = append(list, f.Value)
+	first := queue[0]
 	queue = queue[1:]
 
-	return t.BFSR(queue, list)
+	if first.Left != nil {
+		queue = append(queue, first.Left)
+	}
+	if first.Right != nil {
+		queue = append(queue, first.Right)
+	}
+	list = append(list, first.Value)
+
+	return n.bfsrHelper(queue, list)
 }
 
-// InOrder dfs in order traversal
-func (t *BST) InOrder(n *Node, list *[]int) *[]int {
-	// if ( n == nil) {
-	// 	return list
+// InOrder dfs in order traversal (Left, Root, Right)
+func (n *TreeNode) InOrder() []int {
+	if n == nil {
+		return []int{}
+	}
+	result := []int{}
+	result = append(result, n.Left.InOrder()...)
+	result = append(result, n.Value)
+	result = append(result, n.Right.InOrder()...)
+	return result
+}
+
+// PreOrder dfs pre order traversal (Root, Left, Right)
+func (n *TreeNode) PreOrder() []int {
+	if n == nil {
+		return []int{}
+	}
+	result := []int{}
+	result = append(result, n.Value)
+	result = append(result, n.Left.PreOrder()...)
+	result = append(result, n.Right.PreOrder()...)
+	return result
+}
+
+// PostOrder dfs post order traversal (Left, Right, Root)
+func (n *TreeNode) PostOrder() []int {
+	if n == nil {
+		return []int{}
+	}
+	result := []int{}
+	result = append(result, n.Left.PostOrder()...)
+	result = append(result, n.Right.PostOrder()...)
+	result = append(result, n.Value)
+	return result
+}
+
+// NewTreeNode creates a new TreeNode with the given value
+func NewTreeNode(value int) *TreeNode {
+	return &TreeNode{Value: value}
+}
+
+// Example usage (commented out - uncomment and change package to main to run)
+/*
+func main() {
+	// Create root node
+	tree := NewTreeNode(0)
+	tree.Insert(9)
+	tree.Insert(4)
+	tree.Insert(6)
+	tree.Insert(20)
+	tree.Insert(170)
+	tree.Insert(15)
+	tree.Insert(1)
+
+	// Example: Remove node
+	// err := tree.Remove(1)
+	// if err != nil {
+	//     fmt.Println("Error:", err)
 	// }
-	println("case0")
 
-	if n.Left != nil {
-		println("case1")
-		t.InOrder(n.Left, list)
-	}
+	// Example: Lookup node
+	// node, parent, err := tree.Lookup(4, nil)
+	// if err == nil {
+	//     fmt.Println("Found node:", node.Value)
+	//     if parent != nil {
+	//         fmt.Println("Parent:", parent.Value)
+	//     }
+	// }
 
-	println("case3")
-	*list = append(*list, n.Value)
-	fmt.Println(list)
+	// tree.Print()
 
-	if n.Right != nil {
-		t.InOrder(n.Right, list)
-	}
+	// BFS
+	// bfs := tree.BFS()
+	// fmt.Println("BFS:", bfs)
 
-	return list
+	// BFSR
+	// bfsr := tree.BFSR()
+	// fmt.Println("BFSR:", bfsr)
+
+	// DFS
+	inOrder := tree.InOrder()
+	preOrder := tree.PreOrder()
+	postOrder := tree.PostOrder()
+	fmt.Println("InOrder:", inOrder)
+	fmt.Println("PreOrder:", preOrder)
+	fmt.Println("PostOrder:", postOrder)
 }
-
-// PreOrder dfs pre order traversal
-func (t *BST) PreOrder(n *Node, list *[]int) *[]int {
-	*list = append(*list, n.Value)
-
-	if n.Left != nil {
-		t.PreOrder(n.Left, list)
-	}
-
-	if n.Right != nil {
-		t.PreOrder(n.Right, list)
-	}
-
-	return list
-}
-
-// PostOrder dfs pre order traversal
-func (t *BST) PostOrder(n *Node, list *[]int) *[]int {
-	if n.Left != nil {
-		t.PostOrder(n.Left, list)
-	}
-
-	if n.Right != nil {
-		t.PostOrder(n.Right, list)
-	}
-
-	*list = append(*list, n.Value)
-
-	return list
-}
+*/
