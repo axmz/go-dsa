51c275351d4cf9979c7b1d7758913176420fce11
Alexandr Moroz <afmoroz@gmail.com>
2026-01-17 16:25:10 +0000

    154. Find Minimum in Rotated Sorted Array II

---
+++ leetcode/154.FindMinimuminRotatedSortedArrayII/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func findMin(nums []int) int {
+	low, high := 0, len(nums)-1
+
+	for low < high {
+		pivot := low + (high-low)/2
+		if nums[pivot] < nums[high] {
+			high = pivot
+		} else if nums[pivot] > nums[high] {
+			low = pivot + 1
+		} else {
+			high -= 1
+		}
+	}
+	return nums[low]
+}
+
+func main() {
+	x := 0
+	nums := []int{}
+	fmt.Println(x, nums)
+}
