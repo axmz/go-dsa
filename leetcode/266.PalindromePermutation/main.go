193437d559d42af8f2ad25450ad94c72f8276765
Alexandr Moroz <afmoroz@gmail.com>
2026-01-30 15:10:18 +0000

    266. Palindrome Permutation

--- leetcode/266.PalindromePermutation/main.go
+++ leetcode/266.PalindromePermutation/main.go
@@ -1,7 +1,25 @@
-package main;
+package main
+
+import "fmt"
+
+func canPermutePalindrome(s string) bool {
+	l := len(s)
+	charCount := make(map[byte]int)
+	oddCount := 0
+	for i := 0; i < l; i++ {
+		charCount[s[i]]++
+		if charCount[s[i]]%2 == 1 {
+			oddCount++
+		} else {
+			oddCount--
+		}
+	}
+
+	return oddCount <= 1
+}
 
 func main() {
-	x:= 0;
-	nums := []int{};
+	x := 0
+	nums := []int{}
 	fmt.Println(x, nums)
-};
+}
