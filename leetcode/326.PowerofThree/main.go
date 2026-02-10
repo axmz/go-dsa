3b96dd4e32e8cba0d6c199739039d03016fa6a88
Alexandr Moroz <afmoroz@gmail.com>
2026-02-10 13:14:17 +0000

    326. Power of Three

---
+++ leetcode/326.PowerofThree/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func isPowerOfThree2(n int) bool {
+	if n <= 0 {
+		return false
+	}
+	m := float64(n)
+	log := math.Log10(m) / math.Log10(3)
+	epsilon := 1e-10
+	return math.Abs(log-math.Round(log)) < epsilon
+}
+
+// 1162261467 is the max number that fit int32 that is 3^19
+// anything lower than that, divisible by 3 is the power of 3
+func isPowerOfThree(n int) bool {
+	return n > 0 && 1162261467%n == 0
+}
+
+func isPowerOfThree3(n int) bool {
+	if n == 1 {
+		return true
+	}
+	if n < 3 {
+		return false
+	}
+	for n%3 == 0 {
+		n /= 3
+	}
+	return n == 1
+}
+
+func main() {
+	x := 27
+	fmt.Println(isPowerOfThree(x))
+}
