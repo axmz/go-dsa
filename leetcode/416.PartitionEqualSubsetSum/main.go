fa0a2d1846c7089da726eb1261d16aae1429f442
Alexandr Moroz <afmoroz@gmail.com>
2026-04-11 06:45:48 +0000

    416. Partition Equal Subset Sum

---
+++ leetcode/416.PartitionEqualSubsetSum/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func canPartition(nums []int) bool {
+	total_sum := 0
+	for _, num := range nums {
+		total_sum += num
+	}
+
+	if total_sum%2 != 0 {
+		return false
+	}
+
+	target := total_sum / 2
+	dp := make([]bool, target+1)
+	dp[0] = true
+
+	for _, num := range nums {
+		for j := target; j >= num; j-- {
+			dp[j] = dp[j] || dp[j-num]
+		}
+	}
+
+	return dp[target]
+}
+
+func main() {
+	x := 0
+	nums := []int{}
+	fmt.Println(x, nums)
+}
