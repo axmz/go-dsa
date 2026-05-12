22ac9ac9d3d8c442b3c1dd050fc3cc46194d8893
Alexandr Moroz <afmoroz@gmail.com>
2026-05-12 11:07:24 +0000

    1325. Delete Leaves With a Given Value

---
+++ leetcode/1325.DeleteLeavesWithaGivenValue/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func removeLeafNodes(root *TreeNode, target int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left = removeLeafNodes(root.Left, target)
+	root.Right = removeLeafNodes(root.Right, target)
+	if root.Left == nil && root.Right == nil && root.Val == target {
+		return nil
+	}
+	return root
+}
+
+func main() {
+	x := 0
+	nums := []int{}
+	fmt.Println(x, nums)
+}
