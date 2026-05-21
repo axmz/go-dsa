ae31543d2e366a67ed33210a78c1493c526ced8f
Alexandr Moroz <afmoroz@gmail.com>
2026-05-21 02:18:52 +0000

    860. Lemonade Change

---
+++ leetcode/860.LemonadeChange/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func lemonadeChange(bills []int) bool {
+
+	five, ten := 0, 0
+	for _, bill := range bills {
+		switch bill {
+		case 5:
+			five++
+		case 10:
+			if five == 0 {
+				return false
+			}
+			five--
+			ten++
+		case 20:
+			if ten > 0 && five > 0 {
+				ten--
+				five--
+			} else if five >= 3 {
+				five -= 3
+			} else {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func main() {
+	x := 0
+	nums := []int{}
+	fmt.Println(x, nums)
+}
